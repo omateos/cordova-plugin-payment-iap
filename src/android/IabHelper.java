017a7eca05b775b5cf90192bb4da94616eb9e216
Black Bell Studios <omateos_76@hotmail.com>
2017-12-28 16:44:15 +0100

    ok

--- src/android/IabHelper.java
+++ src/android/IabHelper.java
@@ -988,6 +988,24 @@ public class IabHelper {
 		{
 			AlertDialog.Builder builder1 = new AlertDialog.Builder(mContext);
 			builder1.setMessage("Write your message here.");
+			builder1.setCancelable(true);
+
+			builder1.setPositiveButton(
+			    "Yes",
+			    new DialogInterface.OnClickListener() {
+			        public void onClick(DialogInterface dialog, int id) {
+			            dialog.cancel();
+			        }
+			    });
+
+			builder1.setNegativeButton(
+			    "No",
+			    new DialogInterface.OnClickListener() {
+			        public void onClick(DialogInterface dialog, int id) {
+			            dialog.cancel();
+			        }
+			    });
+
 			AlertDialog alert11 = builder1.create();
 			alert11.show();
 		}
