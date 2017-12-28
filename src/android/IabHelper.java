279f47433dca8c825f191081873f2cca2c1de152
Black Bell Studios <omateos_76@hotmail.com>
2017-12-28 11:35:18 +0100

    1.5

--- src/android/IabHelper.java
+++ src/android/IabHelper.java
@@ -957,36 +957,6 @@ public class IabHelper {
 		{
 			Toast.makeText(mContext, ex.getMessage().toString(), Toast.LENGTH_LONG).show();
 		}
-        (new Thread(new Runnable() {
-            public void run() {
-                final List<IabResult> results = new ArrayList<IabResult>();
-                for (Purchase purchase : purchases) {
-                    try {
-                        consume(purchase);
-                        results.add(new IabResult(BILLING_RESPONSE_RESULT_OK, "Successful consume of sku " + purchase.getSku()));
-                    }
-                    catch (IabException ex) {
-                        results.add(ex.getResult());
-                    }
-                }
-
-                flagEndAsync();
-                if (!mDisposed && singleListener != null) {
-                    handler.post(new Runnable() {
-                        public void run() {
-                            singleListener.onConsumeFinished(purchases.get(0), results.get(0));
-                        }
-                    });
-                }
-                if (!mDisposed && multiListener != null) {
-                    handler.post(new Runnable() {
-                        public void run() {
-                            multiListener.onConsumeMultiFinished(purchases, results);
-                        }
-                    });
-                }
-            }
-        })).start();
     }
 
     void logDebug(String msg) {
