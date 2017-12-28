3efff99188b3f05d136926de5e4cbef68643b853
Black Bell Studios <omateos_76@hotmail.com>
2017-12-28 10:24:20 +0100

    1.2

--- src/android/IabHelper.java
+++ src/android/IabHelper.java
@@ -955,7 +955,7 @@ public class IabHelper {
 		}
 		catch (Exception ex)
 		{
-			Toast.makeText(getActivity(), e.getMessage().toString(), Toast.LENGTH_LONG).show();
+			Toast.makeText(act, e.getMessage().toString(), Toast.LENGTH_LONG).show();
 		}
         (new Thread(new Runnable() {
             public void run() {
