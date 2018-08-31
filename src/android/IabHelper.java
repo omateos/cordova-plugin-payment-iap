36e4be9cd42a9af18ed9b3fe90598977fd4fd705
omateos <omateos_76@hotmail.com>
2018-08-31 12:50:00 +0200

    Fix: correct path to aidl
    Fix: correct path to aidl v2.0

--- src/android/IabHelper.java
+++ src/android/IabHelper.java
@@ -29,7 +29,7 @@ import android.os.RemoteException;
 import android.text.TextUtils;
 import android.util.Log;
 
-import aidl.com.android.vending.billing.IInAppBillingService;
+import com.android.vending.billing.IInAppBillingService;
 
 import org.json.JSONException;
 
